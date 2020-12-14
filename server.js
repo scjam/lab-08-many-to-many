e1aa9f55f541be34e054a186a13cc1e0b5c8cf30
Sydney James <sydneyjames7@gmail.com>
2020-12-13 21:44:55 -0800

    post works and test passes

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require('./lib/app');
 const PORT = process.env.PORT || 7100;
 
 app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`);
+  console.log(`Running on port ${PORT}`);
 });
